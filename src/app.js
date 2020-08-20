4dfea19791f768e7950d75e3b0c45359486515e5
Oscar Landmark <oscarklm@MacBook-Pro.local>
2020-08-20 11:48:48 +0200

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -9,40 +9,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: 'Water bill',
-    note: 'This has been paid.',
-    amount: 4500,
-    createdAt: 450
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: 'Gas bill',
-    note: 'From the new house...',
-    amount: 1500,
-    createdAt: 650
-  })
-);
-store.dispatch(
-  addExpense({
-    description: 'Rent',
-    note: 'Summer Villa.',
-    amount: 109500,
-    createdAt: 250
-  })
-);
-store.dispatch(
-  addExpense({
-    description: 'Pizza',
-    note: 'Yummy!.',
-    amount: 925,
-    createdAt: 250
-  })
-);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
